9343b3604d5015f9dfc8a6a32857733157704164
Dimitriy200 <dikorpshy@mail.com>
2025-10-13 12:45:25 +0300

    add logs configs

--- src/config.py
+++ src/config.py
@@ -6,13 +6,9 @@
 import os
 import logging
 
+from pathlib import Path
 from dotenv import load_dotenv
 
-
-logging.basicConfig(level=logging.INFO,
-                    # filename=os.path.join(os.path.abspath("preprocess-data"),"src", "prepData", "logs", "prep_data_logs.log" ),
-                    filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
 
 load_dotenv()
 
@@ -23,16 +19,29 @@ PATH_TRAIN_FINAL = os.getenv('PATH_TRAIN_FINAL')
 
 PATH_TRAIN_ADD_RAW = os.getenv('PATH_TRAIN_ADD_RAW')
 PATH_TRAIN_ADD_FINAL = os.getenv('PATH_TRAIN_ADD_FINAL')
+PATH_LOG = os.getenv("PATH_LOG")
+
+base_logs_path = Path(PATH_LOG)
 
 paths = [
     PATH_TRAIN_RAW,
     PATH_TRAIN_PROCESSED,
     PATH_TRAIN_FINAL,
     PATH_TRAIN_ADD_RAW,
-    PATH_TRAIN_ADD_FINAL
+    PATH_TRAIN_ADD_FINAL,
+    PATH_LOG
 ]
 
 
 if __name__ == '__main__':
     # Проверка существования дирректорий
     [os.mkdir(path) for path in paths if not os.path.isdir(path)]
+
+    logging.basicConfig(
+        level = logging.INFO,
+        filename =  base_logs_path.joinpath('logs.log'),
+        filemode = "w",
+        format = "%(asctime)s %(levelname)s %(message)s"
+    )
+
+    logging.info("CONFIG COMPLETE")
