d6a6e9643816963edb737eb3f9406e908d2ae3d9
ZakharovAW-Work <zakharovaw2@gmail.com>
2025-10-29 17:31:37 +0300

    ch preprocess

--- src/preprocessing/preprocessing_train/preprocessing.py
+++ src/preprocessing/preprocessing_train/preprocessing.py
@@ -6,12 +6,26 @@
 #   ДАЛЕЕ ПРОИСХОДИТ РАЗДЕЛЕНИЕ ДАННЫХ НА НОРМАЛЬНЫЕ И АНОМАЛЬНЫЕ.
 #   
 # ======================================================
-
+import pandas as pd
 
 from typing import Dict, List, Any
+from sklearn.pipeline import Pipeline
 
 
 class Preprocess:
-    def __init__(self):
-        pass
-        # self.pipeline = 
+    
+    def __init__(self, pipeline: Pipeline  = None):
+        
+        if pipeline is None:
+            self.pipeline = Pipeline()
+        else:
+            self.pipeline = pipeline
+    
+    # ======================================================
+    def fit_pipeline(pipeline: Pipeline, dtaframe: pd.DataFrame):
+        '''
+        Пайплайн должен обучатья только на полном наборе данных
+        '''
+        return pipeline.fit(dtaframe)
+    
+    # ======================================================
