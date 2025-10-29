533fe58d7128e99e415106fd0d10e3c27a4ca358
Dimitriy200 <dikorpshy@mail.com>
2025-10-29 21:59:44 +0300

    hand merge

--- src/tests/test_preprocess.py
+++ src/tests/test_preprocess.py
@@ -1,4 +1,31 @@
 import pandas as pd
 
 
-print(pd.date_range(1, 2))
+import pandas as pd
+
+pd_arr = [
+    pd.DataFrame(
+        [
+            [1, 2, 3],
+            [5, 4, 3]
+        ]
+    ),
+
+    pd.DataFrame(
+        [
+            [10, 20, 30],
+            [50, 40, 30]
+        ]
+    ),
+
+    pd.DataFrame(
+        [
+            [100, 200, 300],
+            [500, 400, 300]
+        ]
+    )
+]
+
+print(pd_arr)
+print()
+print(pd_arr[2])
