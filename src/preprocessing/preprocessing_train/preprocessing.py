1aa6f02f9205475d899de199032762428dc873f8
Dimitriy200 <dikorpshy@mail.com>
2025-10-30 22:44:39 +0300

    ch preprocessing

--- src/preprocessing/preprocessing_train/preprocessing.py
+++ src/preprocessing/preprocessing_train/preprocessing.py
@@ -7,9 +7,13 @@
 #   
 # ======================================================
 import pandas as pd
+import os
+import pickle
 
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Tuple
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 
 class Preprocess:
@@ -22,10 +26,42 @@ class Preprocess:
             self.pipeline = pipeline
     
     # ======================================================
-    def fit_pipeline(pipeline: Pipeline, dtaframe: pd.DataFrame):
+    def different_train_test(
+              self,
+              dtaframe: pd.DataFrame, 
+              save_directory: str = None
+            
+            ) ->  tuple[pd.DataFrame, pd.DataFrame] | None:
         '''
-        Пайплайн должен обучатья только на полном наборе данных
+        Разделяет данные на TRAIN и TEST выборки.
+        Если указан параметри save_directory - сохраняет в формат .csv, иначе возвращает в качестве Pandas наборов.
         '''
+        Train, Test = train_test_split(dtaframe)
+
+
+    # ======================================================
+    def fit_pipeline(
+            self,
+            dtaframe: pd.DataFrame, 
+            pipeline: Pipeline = None
+        ) -> Pipeline | None:
+        '''
+        Пайплайн обучается только на полном наборе данных.
+        '''
+
+        if pipeline is None:
+            pipeline = Pipeline()
+        
         return pipeline.fit(dtaframe)
+    
+
+    def save_pipeline(self, save_pipeline_directory: str, pipeline: Pipeline) -> None:
+        '''
+        Сохраняет pipeline в указанную дирректорию.
+        Метод не знает o существовании указанной директории. 
+        Убедитесь, что перед запуском был запущен config.py из которого можно получить путь по директории pipeline.
+        '''
+        with open(save_pipeline_directory, 'wb') as handle:
+                    save_pik_pipeline = pickle.dumps(pipeline)
     
     # ======================================================
