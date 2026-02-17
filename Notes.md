9f96233fb55546ba6436386bad78d4d45c8f425e
Dimitriy200 <dikorpshy@mail.com>
2026-02-17 23:48:57 +0300

    tested train

--- Notes.md
+++ Notes.md
@@ -10,6 +10,9 @@ cd src/tests
 ```bash
 python -m test_... .py
 ```
+```bash
+pip freeze > requirements.txt
+```
 
 **Тестирование модулей**
 
@@ -18,3 +21,8 @@ python -m test_... .py
 parent_dir = Path(__file__).parent.parent
 sys.path.append(str(parent_dir))
 ```
+
+$$
+f(x) = x^2
+$$
+
--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -2,7 +2,7 @@ import keras
 from keras.metrics import MeanAbsoluteError, RootMeanSquaredError
 
 
-def create_default_autoencoder(input_dim: int = 26) -> keras.Model:
+def create_default_autoencoder(input_dim: int = 27) -> keras.Model:
     model = keras.Sequential([
         keras.layers.Dense(input_dim, activation='elu', input_shape=(input_dim,)),
         keras.layers.Dense(16, activation='elu'),
