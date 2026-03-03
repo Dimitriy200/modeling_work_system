50811756e6e34305665c327181dd9d8440021f58
Dimitriy200 <dikorpshy@mail.com>
2026-03-04 00:01:50 +0300

    del logs

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -11,65 +11,6 @@ from .metrics import compute_rmse
 from .thresholding import choose_optimal_threshold
 
 
-# def train_model(
-#     train_df: np.ndarray,
-#     test_df: np.ndarray,
-#     valid_df: np.ndarray,
-#     anomal_df: np.ndarray,
-
-#     experiment_name: str,
-#     registered_model_name: str,
-    
-#     model:  keras.Model,
-#     epochs: int = 10,
-#     batch_size: int = 80
-#     ):
-    
-#     # Настройка MLflow
-#     mlflow.set_experiment(experiment_name)
-#     mlflow.keras.autolog()
-
-#     with mlflow.start_run() as run:
-#         # Обучение
-#         history = model.fit(
-#             train_df, train_df,
-#             validation_data = (test_df, test_df),
-#             epochs = epochs,
-#             batch_size = batch_size,
-#             shuffle = True
-#         )
-
-#         # Предсказание
-#         X_recon = model.predict(valid_df)
-
-#         # Метрики
-#         rmse = compute_rmse(valid_df, X_recon)
-#         mlflow.log_metric("rmse", rmse)
-#         mlflow.log_param("epochs", epochs)
-
-#         # Логирование модели
-#         signature = infer_signature(train_df, model.predict(train_df[:10]))
-#         mlflow.keras.log_model(
-#             model,
-#             artifact_path = "autoencoder",
-#             registered_model_name = registered_model_name,
-#             signature = signature
-#         )
-
-
-#         optimal_line, optimal_df = choose_optimal_threshold(
-#             model = model,
-#             normal_control_df = valid_df,
-#             anomaly_control_df = anomal_df,
-#             )
-
-#         logging.info(optimal_df)
-
-#     # run_id = run.info.run_id
-#     return model, optimal_df
-
-
-
 def train_model(
     model: keras.Model,
     train_df: np.ndarray,
