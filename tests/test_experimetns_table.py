956af63347f211dfebe81b2c7ba942340452b6f3
ZakharovAW-Work <zakharovaw2@gmail.com>
2026-06-03 15:20:39 +0300

    add plot history

--- tests/test_experimetns_table.py
+++ tests/test_experimetns_table.py
@@ -3,6 +3,7 @@ import numpy as np
 import logging
 
 import sys
+import os
 from pathlib import Path
 parent_dir = Path(__file__).parent.parent
 sys.path.append(str(parent_dir))
@@ -10,19 +11,24 @@ sys.path.append(str(parent_dir))
 from modeling_work_system.preprocessing.preprocessing import Preprocess
 from modeling_work_system.pipeline.pipeline_fit import PipelineFit
 from modeling_work_system.preprocessing.scaler import Scaler
-from modeling_work_system.preprocessing.load_data_add import LoadDataTrainAdd
 from modeling_work_system.preprocessing.load_data_first import LoadDataTrain
+
+from modeling_work_system.models.autoencoders.autoencoder import AutoEncoder
+from modeling_work_system.models.core_models.autoencoders import STANDART_AE, EXPANSION_AE
+
 from modeling_work_system.mlflowservice.mlflowservice import Mlflowservice
 from modeling_work_system.metrics.metrics import ExperimentMetric
 from modeling_work_system.metrics.aemetrics import AEMetricResult
-from modeling_work_system.models.autoencoders.autoencoder import AutoEncoder
+
 from modeling_work_system.metrics.compose_table_metrics import (
     run_reconstruction_comparison_table, 
     run_classification_comparison_table)
 from modeling_work_system.metrics.statistic_compare import paired_t_test
+
 from modeling_work_system.config import (
     PATH_LOG,
     PATH_SKALERS,
+    PATH_IMG,
 
     PATH_TRAIN_RAW,
     PATH_TRAIN_ADD_RAW,
@@ -35,6 +41,7 @@ from modeling_work_system.config import (
     MLFLOW_REPO_PASSWORD
 )
 
+from modeling_work_system.plots.history import plot_training_curves
 
 
 # ======================================================
@@ -44,7 +51,9 @@ loader = LoadDataTrain()
 scaler_manager = Scaler()
 processor = Preprocess()
 metrics = ExperimentMetric()
-model_ae_standart = AutoEncoder()
+
+ae_standart = AutoEncoder(model_core=STANDART_AE)
+ae_expansion = AutoEncoder(model_core=EXPANSION_AE)
 
 
 # ======================================================
@@ -68,4 +77,17 @@ scaled_X_test = scaler_manager.apply_scaler(std_scaler, splited_dataframes['X_te
 # ======================================================
 # II ОБУЧЕНИЕ МОДЕЛЕЙ
 # ======================================================
+train_info = ae_standart.fit(
+    X_train=scaled_X_train,
+    X_val=scaled_X_val,
+    X_test=scaled_X_test,
+    Y_val=splited_dataframes['y_val'],
+    epochs=3)
 
+
+# ======================================================
+# III ВИЗУАЛИЗАЦИЯ РЕЗУЛЬТАТОВ [ОБУЧЕНИЕ / КЛАССИФИКАЦИЯ / ВОССТАНОВЛЕНИЕ ДАННЫХ]
+# ======================================================
+plot_training_curves(
+    history=train_info["history"],
+    save_path=os.path.join(PATH_IMG, "standart_ae_history.png"))
